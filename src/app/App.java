e1f8093548ea159f3f0b184500fb08a0e5094814
Rafael <rafavilvert@hotmai.com>
2021-11-03 18:24:35 -0300

    Fixed double comma

--- src/app/App.java
+++ src/app/App.java
@@ -3,19 +3,39 @@ package app;
 public class App {
 
 	public static void main(String[] args) {
-		
+
 		GameStep game = new GameStep();
-		game.start();
-		game.chooseLevelGame();
-		game.chooseName();
-		game.createHero();
-		game.gameIntroduction();
-		game.goAheadOrGiveUp();
-		game.enterTheDoor();
-		game.chooseADoorToFight();
-//		game.fightHeroAgainstArmeiro();
-//		game.fightHeroAgainstAlquimista();
-//		game.fightHeroAgainstPoderosoChefao();
+
+		try {
+			game.start();
+			Thread.sleep(5000);
+
+			game.chooseLevelGame();
+
+			game.chooseName();
+
+			game.chooseSex();
+
+			game.createHero();
+
+			game.printHero();
+			Thread.sleep(10000);
+
+			game.gameIntroduction();
+			Thread.sleep(10000);
+
+			game.goAheadOrGiveUp();
+			Thread.sleep(10000);
+
+			game.enterTheDoor();
+			Thread.sleep(5000);
+
+			game.chooseADoorToFight();
+		} catch (InterruptedException e) {
+
+			System.out.println("Error");
+		}
+
 	}
 
 }
--- src/gameAction/Fight.java
+++ src/gameAction/Fight.java
@@ -19,7 +19,7 @@ public class Fight {
 		
 
 		while (hero.getLife() > 0.0 && enemy.getLife() > 0.0) {
-			System.out.println("Digite 1 Jodar o dado 2 sair");
+			System.out.println("Digite 1 Jodar o dado 2 sair\n");
 			chooseAtack = sc.nextInt();
 
 			if (chooseAtack == 1) {
@@ -45,30 +45,30 @@ public class Fight {
 
 		System.out.println("Você lançou o dado");
 		System.out.println("Dado heroi " + diceRollHero);
-		System.out.println("Inimigo lançou o dado");
+		System.out.println("Inimigo lançou o dado\n");
 		System.out.println("Dado Inimigo " + diceRollEnemy);
 
 		switch (diceRollHero) {
 		case 1:
-			System.out.println("Vida do heroi " + hero.getLife());
-			System.out.println("O dado caiu em 1 seu ataque foi neutralizado pelo inimigo e não causou dano");
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+			System.out.println("O dado caiu em 1 seu ataque foi neutralizado pelo inimigo e não causou dano\n");
 			enemy.setLife(enemy.getLife());
 			break;
 		case 20:
-			System.out.println("Vida do heroi " + hero.getLife());
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
 			System.out.println("O dado caiu em 20 seu ataque é especial");
 			System.out.println(
-					"Você atacou com a " + hero.getWeaponOne() + " e causou " + "100% de " + "dano no inimigo");
+					"Você atacou com a " + hero.getWeaponOne() + " e causou " + "100% de " + "dano no inimigo\n");
 			enemy.setLife(enemy.getLife() - enemy.getLife());
 			break;
 
 		default:
-			System.out.println("Vida do heroi " + hero.getLife());
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
 			System.out.println("Você atacou com " + hero.getWeaponOne() + " causou um dano de "
-					+ (hero.getTotaldamage() + diceRollHero) + " pontos");
+					+ (hero.getTotaldamage() + diceRollHero) + " pontos\n");
 			enemy.setLife(enemy.getLife() - (hero.getTotaldamage() + diceRollHero));
 			if (enemy.getLife() < 0.0) {
-				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====");
+				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====\n");
 				enemy.setLife(0.0);
 			}
 			break;
@@ -78,33 +78,34 @@ public class Fight {
 
 			switch (diceRollEnemy) {
 			case 1:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não causou dano");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não causou dano\n");
 				hero.setLife(hero.getLife());
 				System.out.println(enemy.getTotaldamage());
-				System.out.println("Vida do inimigo " + enemy.getLife());
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
 				break;
 			case 20:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 20 ataque do inimigo é especial");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 20 ataque do inimigo é especial\n");
 				System.out
-						.println("Inimigo atacou com a " + enemy.getWeaponOne() + " e causou " + "100% de " + "dano ");
+						.println("Inimigo atacou com a " + enemy.getWeaponOne() + " e causou " + "100% de " + "dano \n");
 				hero.setLife(hero.getLife() - hero.getLife());
 				System.out.println(enemy.getTotaldamage());
 				System.out.println(diceRollHero);
-				System.out.println("Vida do heroi " + hero.getLife());
-				System.out.println("Vida do inimigo " + enemy.getLife());
+				System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+				System.out.println("Vida do inimigo " + enemy.getLife() + "\n");
 				System.out.println("Você está morto kkkkkkkkkkk\nGAME OVER!");
 				break;
 
 			default:
-				System.out.println("Vida do Inimigo " + enemy.getLife());
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
 				System.out.println("Inimigo atacou com " + enemy.getWeaponOne() + " causou um dano de "
-						+ ((enemy.getTotaldamage() + diceRollEnemy) * 0.8) + " pontos");
+						+ ((enemy.getTotaldamage() + diceRollEnemy) * 0.8) + " pontos\n");
 
 				hero.setLife(hero.getLife() - ((enemy.getTotaldamage() + diceRollEnemy) * 0.8));
 				if (hero.getLife() < 0.0) {
-					System.out.println("Você morreu " + hero.getName());
+					System.out.println("Você morreu " + hero.getName() + "\n");
+					System.exit(0);
 					hero.setLife(0.0);
 				}
 				System.out.println("Vida Heroi " + hero.getLife());
@@ -118,31 +119,31 @@ public class Fight {
 		diceRollEnemy = random.nextInt(20);
 
 		System.out.println("Você lançou o dado");
-		System.out.println("Dado heroi " + diceRollHero);
+		System.out.println("Dado heroi " + diceRollHero + "\n");
 		System.out.println("Inimigo lançou o dado");
 		System.out.println("Dado Inimigo " + diceRollEnemy);
 
 		switch (diceRollHero) {
 		case 1:
-			System.out.println("Vida do heroi " + hero.getLife());
-			System.out.println("O dado caiu em 1 seu ataque foi neutralizado pelo inimigo e não causou dano");
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+			System.out.println("O dado caiu em 1 seu ataque foi neutralizado pelo inimigo e não causou dano\n");
 			enemy.setLife(enemy.getLife());
 			break;
 		case 20:
-			System.out.println("Vida do heroi " + hero.getLife());
-			System.out.println("O dado caiu em 20 seu ataque é especial");
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+			System.out.println("O dado caiu em 20 seu ataque é especial \n");
 			System.out.println(
 					"Você atacou com a " + hero.getWeaponOne() + " e causou " + "100% de " + "dano no inimigo");
 			enemy.setLife(enemy.getLife() - enemy.getLife());
 			break;
 
 		default:
-			System.out.println("Vida do heroi " + hero.getLife());
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
 			System.out.println("Você atacou com " + hero.getWeaponOne() + " causou um dano de "
-					+ (hero.getTotaldamage() + diceRollHero) + " pontos");
+					+ (hero.getTotaldamage() + diceRollHero) + " pontos\n");
 			enemy.setLife(enemy.getLife() - (hero.getTotaldamage() + diceRollHero));
 			if (enemy.getLife() < 0.0) {
-				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====");
+				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====\n");
 				enemy.setLife(0.0);
 			}
 			break;
@@ -152,28 +153,29 @@ public class Fight {
 
 			switch (diceRollEnemy) {
 			case 1:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não causou dano");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não causou dano\n");
 				hero.setLife(hero.getLife());
 				break;
 			case 20:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 20 ataque do inimigo é especial");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 20 ataque do inimigo é especial\n");
 				System.out
-						.println("Inimigo atacou com a " + enemy.getWeaponOne() + " e causou " + "100% de " + "dano ");
+						.println("Inimigo atacou com a " + enemy.getWeaponOne() + " e causou " + "100% de " + "dano \n");
 				hero.setLife(hero.getLife() - hero.getLife());
-				System.out.println("Vida do heroi " + hero.getLife());
-				System.out.println("Vida do inimigo " + enemy.getLife());
+				System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+				System.out.println("Vida do inimigo " + enemy.getLife() + "\n");
 				System.out.println("Você está morto kkkkkkkkkkk\nGAME OVER!");
 				break;
 
 			default:
-				System.out.println("Vida do Inimigo " + enemy.getLife());
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
 				System.out.println("Inimigo atacou com " + enemy.getWeaponOne() + " causou um dano de "
-						+ (enemy.getTotaldamage() + diceRollEnemy) + " pontos");
+						+ (enemy.getTotaldamage() + diceRollEnemy) + " pontos\n");
 				hero.setLife(hero.getLife() - (enemy.getTotaldamage() + diceRollEnemy));
 				if (hero.getLife() < 0.0) {
 					System.out.println("Você morreu " + hero.getName());
+					System.exit(0);
 					hero.setLife(0.0);
 				}
 				System.out.println("Vida Heroi " + hero.getLife());
@@ -188,31 +190,31 @@ public class Fight {
 		diceRollEnemy = random.nextInt(20);
 
 		System.out.println("Você lançou o dado");
-		System.out.println("Dado heroi " + diceRollHero);
+		System.out.println("Dado heroi " + diceRollHero + "\n");
 		System.out.println("Inimigo lançou o dado");
-		System.out.println("Dado Inimigo " + diceRollEnemy);
+		System.out.println("Dado Inimigo " + diceRollEnemy + "\n");
 
 		switch (diceRollHero) {
 		case 1:
-			System.out.println("Vida do inimigo " + enemy.getLife());
+			System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
 			System.out.println("O dado caiu em 1 Inimigo neutralizou o ataque e não sofreu dano");
 			enemy.setLife(enemy.getLife());
 			break;
 		case 20:
-			System.out.println("Vida do heroi " + hero.getLife());
-			System.out.println("O dado caiu em 20 seu ataque é especial");
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+			System.out.println("O dado caiu em 20 seu ataque é especial\n");
 			System.out.println(
-					"Você atacou com a " + hero.getWeaponOne() + " e causou " + "100% de " + "dano no inimigo");
+					"Você atacou com a " + hero.getWeaponOne() + " e causou " + "100% de " + "dano no inimigo\n");
 			enemy.setLife(enemy.getLife() - enemy.getLife());
 			break;
 
 		default:
-			System.out.println("Vida do heroi " + hero.getLife());
+			System.out.printf("Vida do heroi %.2f \n", hero.getLife());
 			System.out.println("Você atacou com " + hero.getWeaponOne() + " causou um dano de "
-					+ ((hero.getTotaldamage() + diceRollHero) * 0.9) + " pontos");
+					+ ((hero.getTotaldamage() + diceRollHero) * 0.9) + " pontos\n");
 			enemy.setLife(enemy.getLife() - (hero.getTotaldamage() + diceRollHero) * 0.9);
 			if (enemy.getLife() < 0.0) {
-				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====");
+				System.out.println("==== VOCÊ DERROTOU O " + enemy.getName() + " ====\n");
 				enemy.setLife(0.0);
 			}
 			break;
@@ -222,29 +224,30 @@ public class Fight {
 
 			switch (diceRollEnemy) {
 			case 1:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não sofreu dano");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 1 Heroi neutralizou o ataque e não sofreu dano\n");
 				hero.setLife(hero.getLife());
 				System.out.println(enemy.getTotaldamage());
 				break;
 			case 20:
-				System.out.println("Vida do inimigo " + enemy.getLife());
-				System.out.println("O dado caiu em 20 ataque do inimigo é especial");
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
+				System.out.println("O dado caiu em 20 ataque do inimigo é especial\n");
 				System.out
 						.println("Inimigo atacou com a " + enemy.getWeaponOne() + " e causou " + "100% de " + "dano ");
 				hero.setLife(hero.getLife() - hero.getLife());
-				System.out.println("Vida do heroi " + hero.getLife());
-				System.out.println("Vida do inimigo " + enemy.getLife());
+				System.out.printf("Vida do heroi %.2f \n", hero.getLife());
+				System.out.println("Vida do inimigo " + enemy.getLife() + "\n");
 				System.out.println("Você está morto kkkkkkkkkkk\nGAME OVER!");
 				break;
 
 			default:
-				System.out.println("Vida do Inimigo " + enemy.getLife());
+				System.out.printf("Vida do Inimigo %.2f \n", enemy.getLife());
 				System.out.println("Inimigo atacou com " + enemy.getWeaponOne() + " causou um dano de "
-						+ (enemy.getTotaldamage() + diceRollEnemy) + " pontos");
+						+ (enemy.getTotaldamage() + diceRollEnemy) + " pontos\n");
 				hero.setLife(hero.getLife() - (enemy.getTotaldamage() + diceRollEnemy));
 				if (hero.getLife() < 0.0) {
 					System.out.println("Você morreu " + hero.getName());
+					System.exit(0);
 					hero.setLife(0.0);
 				}
 				System.out.println("Vida Heroi " + hero.getLife());
